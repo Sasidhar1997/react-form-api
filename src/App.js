8a1c7dbd22b385a92cd36ca0d2ca719c80174395
sasidhar <sasidhar@gmail.com>
2019-11-26 23:20:53 +0530

    React Form API - Console Log Output

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { name: "", email: "" };
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log("Username :" + this.state.name);
+        console.log("UserEmail :" + this.state.email);
+        const url = "https://jsonplaceholder.typicode.com/users/";
+        const data = { name: this.state.name, email: this.state.email };
+        fetch(url, {
+            method: "POST", // or ‘PUT’
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(res => res.json())
+            .catch(error => console.error("Error:", error))
+            .then(response => console.log("Success:", response));
+    };
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="name"
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        onChange={this.handleChange}
+                    />
+                    <input type="submit" value="Add user" />{" "}
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
